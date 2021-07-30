b8f48282193173ce86af691e10554190a0169d74
ZongXing Hu <huzx@zetyun.com>
2021-07-30 22:02:03 +0800

    [examples] add examples of tail kafka stream and replay wal to disk

--- examples/src/main/java/com/zetyun/example/kafka/KafkaConsumerExample.java
+++ examples/src/main/java/com/zetyun/example/kafka/KafkaConsumerExample.java
@@ -5,22 +5,20 @@ import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
 import org.apache.kafka.common.serialization.ByteArrayDeserializer;
-import org.rocksdb.CloudWalMeta;
-import org.rocksdb.CloudWalUtils;
-import org.rocksdb.RocksDB;
+import org.rocksdb.*;
 
-import java.nio.ByteBuffer;
 import java.time.Duration;
 import java.util.Arrays;
 import java.util.Properties;
 
 public class KafkaConsumerExample {
 
+    private static String db_path = "./examples/data/rocksdb-data";
     static {
         RocksDB.loadLibrary();
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         String bootstrapServers = "172.20.3.83:9092";
         String grp_id = "huzx_consumer_01";
         String topic = "huzx10.rocksdb.cloud.durable.example";
@@ -32,62 +30,20 @@ public class KafkaConsumerExample {
         properties.setProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
         KafkaConsumer<byte[], byte[]> consumer = new KafkaConsumer<byte[], byte[]>(properties);
         consumer.subscribe(Arrays.asList(topic));
+
+        final Options options = new Options().setCreateIfMissing(true);
+        final RocksDB rocksDB = RocksDB.open(options, db_path);
+
         while (true) {
             ConsumerRecords<byte[], byte[]> records = consumer.poll(Duration.ofMillis(10000));
             for (ConsumerRecord<byte[], byte[]> record : records) {
                 CloudWalMeta meta = new CloudWalUtils().extractLogRecord(record.value());
                 System.out.println("xxxx=> Fetch meta is:" +  meta.toString());
-            }
-        }
-    }
-
-    public static int fromByteArray01(byte[] bytes) {
-        return bytes[0] << 24 | (bytes[1] & 0xFF) << 16 | (bytes[2] & 0xFF) << 8 | (bytes[3] & 0xFF);
-    }
-
-    public static int fromByteArray02(byte[] bytes) {
-        return ((bytes[3] & 0xFF) << 24) |
-                ((bytes[2] & 0xFF) << 16) |
-                ((bytes[1] & 0xFF) << 8 ) |
-                ((bytes[0] & 0xFF) << 0 );
-    }
-
-    public static int extractFix32Int(byte[] input) {
-        byte[] fix32Int00 = Arrays.copyOfRange(input, 0, 4);
-        byte[] fix32Int01 =  {input[3], input[2], input[1], input[0]};
-
-        int value00 = KafkaConsumerExample.fromByteArray01(fix32Int00);
-        int value01 = KafkaConsumerExample.fromByteArray02(fix32Int00);
-        System.out.println("1==>value00 =>" + value00 + ", value01:" + value01);
-
-        value00 = KafkaConsumerExample.fromByteArray01(fix32Int01);
-        value01 = KafkaConsumerExample.fromByteArray02(fix32Int01);
-        System.out.println("2==>value00 =>" + value00 + ", value01:" + value01);
-        return value01;
-    }
-
-    public static long extractFix64Long(byte[] input) {
-        byte[] fix64Long = Arrays.copyOfRange(input, 4, 4 + 8);
-        ByteBuffer wrapped = ByteBuffer.wrap(fix64Long);
-        Long value = wrapped.getLong();
-        return value;
-    }
-
-    public static void extractWalLog(byte[] inputBytes) {
-        int opType = extractFix32Int(inputBytes);
-        System.out.println("Current OpType is:" + opType);
-
-        switch (opType) {
-            case 1: {
-                Long position = extractFix64Long(inputBytes);
-                System.out.println("Current OpType is:" + opType + ", Offset of File is:" + position);
-                break;
-            }
-            case 2: {
-                break;
-            }
-            case 4: {
-                break;
+                WriteBatch batch = meta.getBatchRecord();
+                if (batch != null && batch.count() > 0) {
+                    rocksDB.write(new WriteOptions(), batch);
+                    rocksDB.flush(new FlushOptions());
+                }
             }
         }
     }
